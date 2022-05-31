8227744a7b723cb13a2a5e79140d332243bd0207
Rodrigo Oliveira <47666631+Rodrigo-Santoos@users.noreply.github.com>
2022-05-31 15:03:28 -0300

    Enum Java

---
+++ Enumeraçao/src/aplication/Aplication.java
@@ -0,0 +1,36 @@
+package aplication;
+
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
+public class Aplication {
+
+	public static void main(String[] args) {
+
+		Order or = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(or);
+		
+		//convertendo String para enum
+		
+		OrderStatus os1 = OrderStatus.DEVILERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("PROCESSIG");
+		
+		System.out.println(os1);
+		System.out.println(os2);
+		System.out.println();
+		
+		// verificando o tipo da variavel
+		System.out.println(os1.getClass());
+		System.out.println(os2.getClass());
+		
+		
+		
+		
+		
+	}
+
+}
